0f42789ea5add0e42ea177339a509f2c4be84a9e
Ryan Weaver <ryan@thatsquality.com>
2010-04-24 18:11:55 -0500

    Addition of the app.yml configuration (mostly just comments) and the Doctrine behavior, which unlinks relations.

---
+++ lib/behavior/listener/sfInlineObjectTemplateListener.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Listener class for sfInlineObjectTemplate
+ * 
+ * @package     sfInlineObjectPlugin
+ * @subpackage  behavior
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ */
+
+class sfInlineObjectTemplateListener extends Doctrine_Record_Listener
+{
+  public function postInsert(Doctrine_Event $event)
+  {
+    $event->getInvoker()->deleteLinkAndAssetReferences();
+  }
+
+  public function postUpdate(Doctrine_Event $event)
+  {
+    $event->getInvoker()->deleteLinkAndAssetReferences();
+  }
+
+  /**
+   * Removes all references to related objects that are as a result of
+   * inline object references
+   * 
+   * This guarantees that if a reference to a related object is removed
+   * from an inline text field, that relation won't continue to exist forever.
+   * In that sense, this is effectively a "cache clear" for these relations.
+   */
+  protected function _deleteInlineObjectReferences(Doctrine_Event $event)
+  {
+    $modelConfig = sfConfig::get('app_inline_object_relations', array());
+    $modelName = get_clas($event->getInvoker());
+
+    // Find the array of relations on this model that are InlineObject relations
+    $relations = isset($models[$modelName]) ? $models[$modelName] : array();
+    foreach ($relations as $relation)
+    {
+      // Unlink ALL related objects on the given relation
+      $event->getInvoker()->unlink($relation, array(), true);
+    }
+  }
+}
---
+++ lib/behavior/sfInlineObjectTemplate.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Allows a Doctrine_Record to embed foreign-related objects in text fields
+ * while keeping real relations and avoiding extra queries
+ * 
+ * @package     sfInlineObjectPlugin
+ * @subpackage  behavior
+ * @author      Ryan Weaver <ryan@thatsquality.com>
+ */
+
+class sfInlineObjectTemplate extends Doctrine_Template
+{
+
+  /**
+   * Set the table definition for sfInlineObjectTemplate
+   */
+  public function setTableDefinition()
+  {
+    $this->addListener(new sfInlineObjectTemplateListener());
+  }
+}
