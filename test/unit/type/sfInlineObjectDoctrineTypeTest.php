e39480027bf501617e02bfda3dae8d793f1611fd
weaverryan <ryan@thatsquality.com>
2010-06-11 08:36:40 -0500

    [test] No real changes, modified whitespace.

--- test/unit/type/sfInlineObjectDoctrineTypeTest.php
+++ test/unit/type/sfInlineObjectDoctrineTypeTest.php
@@ -3,18 +3,21 @@
 require dirname(__FILE__).'/../../bootstrap/doctrine.php';
 
 $t = new lime_test(2);
-Doctrine_Core::getTable('Product')->createQuery('p')->delete()->execute();
 
-// Instantiate what will essentially be our "stub".
-$inlineProduct = new InlineObjectProduct('product', array(
-  'model'      => 'Product',
-  'key_column' => 'slug',
-));
-$t->is($inlineProduct->getRelatedObject('test-product'), false, '->getRelatedObject(test-product) returns null if the object is not found');
+$t->info('1 - Test ->getRelatedObject()';)
+  Doctrine_Core::getTable('Product')->createQuery('p')->delete()->execute();
 
-$product = new Product();
-$product->title = 'Test Product';
-$product->slug = 'test-product';
-$product->save();
+  // Instantiate what will essentially be our "stub".
+  $inlineProduct = new InlineObjectProduct('product', array(
+    'model'      => 'Product',
+    'key_column' => 'slug',
+  ));
+  $t->is($inlineProduct->getRelatedObject('test-product'), false, '->getRelatedObject(test-product) returns null if the object is not found');
 
-$t->is($inlineProduct->getRelatedObject('test-product')->id, $product->id, '->getRelatedObject() returns the correct related object');
+  $product = new Product();
+  $product->title = 'Test Product';
+  $product->slug = 'test-product';
+  $product->save();
+
+  $t->is($inlineProduct->getRelatedObject('test-product')->id, $product->id, '->getRelatedObject() returns the correct related object');
+
